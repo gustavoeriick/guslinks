fded1f7b21975318770252e104299d966361aebb
Gustavo Ferreira <gustavo.eriick@gmail.com>
2024-04-01 17:03:43 -0300

    att - 01042024 17h03m

--- obj/Debug/net8.0/guslinks.AssemblyInfo.cs
+++ obj/Debug/net8.0/guslinks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guslinks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed2b416dab46c4f70bb0a6057776f7240d570a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("guslinks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guslinks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
